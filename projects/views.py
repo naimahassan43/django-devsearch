ee486aaa28455b7a95c37bd1bbe4b56fefe3b35a
Naima Hassan <naima.hassan434@gmail.com>
2021-12-16 00:52:25 +0000

    urls and views setup for projects app

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def projects(request):
+    return HttpResponse('Here are our products')
+def project(request,pk):
+    return HttpResponse('Here is our single product' +' '+ str(pk))
 # Create your views here.
