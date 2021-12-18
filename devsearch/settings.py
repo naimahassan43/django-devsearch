7c695022bf1ee805f9dbf4cd28031a3839cbe940
Naima Hassan <naima.hassan434@gmail.com>
2021-12-18 01:05:57 +0000

    Static file: upload image from user & install whitenoise

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!x&%-!5f8ncyks4tvf1_!!sj!otl4$@wzisab)&p+f%gc8=h5!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,13 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
             BASE_DIR/'static'
         ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- projects/forms.py
+++ projects/forms.py
@@ -4,4 +4,4 @@ from .models import Projects
 class ProjectForm(ModelForm):
  class Meta:
   model = Projects
-  fields = ['title','description','demo_link','source_link','tags']
+  fields = ['title','featured_image','description','demo_link','source_link','tags']
